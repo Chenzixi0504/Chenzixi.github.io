36697e6b47133b5b577a3585e58a0eb8c6aa3bb1
陈梓禧 <112500321+Chenzixi0504@users.noreply.github.com>
2023-09-13 23:18:15 +0800

    Add files via upload

---
+++ Home/data.source.js
@@ -0,0 +1,713 @@
+import React from 'react';
+export const Nav32DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper lmhkkx5609-editor_css' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header3-logo',
+    children:
+      'https://raw.githubusercontent.com/Chenzixi0504/Chenzixi0504.github.io/main/7c63b308d4b3.png',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item1',
+        className: 'header3-item lmhkrbi3zjk-editor_css',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <span>
+                        <span>
+                          <p>
+                            <span>
+                              <b>个人介绍</b>
+                            </span>
+                          </p>
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              ),
+              name: 'text',
+              className: 'lmhkayz4roo-editor_css',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item~lmhkcxb1sau',
+        className: 'header3-item lmhkrdjw2y-editor_css',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <p>
+                        <b>作品集</b>
+                      </p>
+                    </span>
+                  </span>
+                </span>
+              ),
+              name: 'text',
+              className: 'lmhkayz4roo-editor_css',
+            },
+          ],
+        },
+        subItem: null,
+      },
+      {
+        name: 'item~lmhkup2pwqb',
+        className: 'header3-item lmhkrdjw2y-editor_css',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <span>
+                    <p>
+                      <b>联系方式</b>
+                    </p>
+                  </span>
+                </span>
+              ),
+              name: 'text',
+              className: 'lmhkayz4roo-editor_css',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Content110DataSource = {
+  OverPack: {
+    className: 'home-page-wrapper content11-wrapper lmhkkuzuy4m-editor_css',
+    playScale: 0.3,
+    always: false,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children: '',
+        className: 'title-image lmh5xbupzb-editor_css',
+      },
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <span>
+                        <span>
+                          <span>
+                            <span>
+                              <span>
+                                <p>欢迎来到陈梓禧的个人网站</p>
+                              </span>
+                            </span>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <span>
+                        <span>
+                          <span>
+                            <span>
+                              <span>
+                                <span>
+                                  <span>
+                                    <p>个人介绍</p>
+                                  </span>
+                                </span>
+                              </span>
+                            </span>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'title-content lmh60v8t9c-editor_css',
+      },
+      {
+        name: 'content2',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <p>有较强的学习钻研能力，对前沿技术感兴趣。</p>
+                  <p>有良好的动手能力，喜欢工程实践 试错能力较高。</p>
+                  <p>
+                    有良好的组织管理能力，多次主动担任团队队长，擅长信息整合。
+                  </p>
+                  <p>热爱生活 性格乐观，擅长思考，喜欢分享知识。</p>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'title-content',
+      },
+    ],
+  },
+  button: {
+    className: '',
+    children: {
+      a: {
+        className: 'button lmh7nopim18-editor_css',
+        href: '#',
+        children: (
+          <span>
+            <span>
+              <p>
+                <br />
+              </p>
+            </span>
+          </span>
+        ),
+      },
+    },
+  },
+};
+export const Pricing00DataSource = {
+  wrapper: { className: 'home-page-wrapper pricing0-wrapper' },
+  OverPack: { playScale: 0.3, className: 'home-page pricing0', always: false },
+  imgWrapper: { className: 'pricing0-img-wrapper', md: 12, xs: 24 },
+  img: {
+    className: 'pricing0-img',
+    name: 'image',
+    children:
+      'https://raw.githubusercontent.com/Chenzixi0504/Chenzixi0504.github.io/main/AAAAABjARQnAQ.png',
+  },
+  childWrapper: {
+    className: 'pricing0-text-wrapper',
+    md: 12,
+    xs: 24,
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <p>
+                      <br />
+                    </p>
+                    <p>
+                      <span>第五届世界渲染大赛“移动冥想“参赛作品</span>
+                      <br />
+                    </p>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-title lmh97nerqkt-editor_css',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <span>
+                        <span>
+                          <span>
+                            <p>
+                              对UE引擎有初步认识，深入学习与研究入门案例后尝试制作动画
+                            </p>
+                            <p>
+                              <br />
+                            </p>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-content lmh99bqgd6b-editor_css',
+      },
+      {
+        name: 'button',
+        children: {
+          href:
+            'https://www.bilibili.com/video/BV1Zd4y1w78y/?spm_id_from=333.999.0.0&vd_source=9a8e9fb48f82546fa4a631dba8d8357d',
+          type: 'primary',
+          children: (
+            <span>
+              <span>
+                <p>点击跳转</p>
+              </span>
+            </span>
+          ),
+          target: '_blank',
+          className: 'lmh99wvae5f-editor_css',
+        },
+        className: 'lmh8gxa7dk8-editor_css',
+      },
+    ],
+  },
+};
+export const Pricing01DataSource = {
+  wrapper: { className: 'home-page-wrapper pricing0-wrapper' },
+  OverPack: { playScale: 0.3, className: 'home-page pricing0', always: false },
+  imgWrapper: { className: 'pricing0-img-wrapper', md: 12, xs: 24 },
+  img: {
+    className: 'pricing0-img',
+    name: 'image',
+    children:
+      'https://raw.githubusercontent.com/Chenzixi0504/Chenzixi0504.github.io/main/AAAAAAAAAAABjARQnAQ.png',
+  },
+  childWrapper: {
+    className: 'pricing0-text-wrapper',
+    md: 12,
+    xs: 24,
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <p>
+                        <br />
+                      </p>
+                      <p>风格化渲染与实现模型内外描边</p>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-title lmhli86139p-editor_css',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <p>
+                    对UE引擎中后期盒子模块进行学习，尝试对模型内外描边的实现<br />
+                  </p>
+                  <p>
+                    <br />
+                  </p>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-content lmhlio7gym-editor_css',
+      },
+      {
+        name: 'button',
+        children: {
+          href:
+            'https://www.bilibili.com/video/BV1ye411K7fq/?spm_id_from=333.999.0.0&vd_source=9a8e9fb48f82546fa4a631dba8d8357d',
+          type: 'primary',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <p>点击跳转</p>
+                </span>
+              </span>
+            </span>
+          ),
+          target: '_blank',
+        },
+        className: 'lmhlivp0e5-editor_css',
+      },
+    ],
+  },
+};
+export const Pricing02DataSource = {
+  wrapper: { className: 'home-page-wrapper pricing0-wrapper' },
+  OverPack: { playScale: 0.3, className: 'home-page pricing0', always: false },
+  imgWrapper: { className: 'pricing0-img-wrapper', md: 12, xs: 24 },
+  img: {
+    className: 'pricing0-img',
+    name: 'image',
+    children:
+      'https://raw.githubusercontent.com/Chenzixi0504/Chenzixi0504.github.io/main/AABjARQnAQ.png',
+  },
+  childWrapper: {
+    className: 'pricing0-text-wrapper',
+    md: 12,
+    xs: 24,
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <p>
+                      <br />
+                    </p>
+                    <p>还原带热浪风沙的沙漠</p>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-title lmhp2jio7wg-editor_css',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <span>
+                      <p>
+                        对UE引擎中后期盒子模块进行学习并尝试对画面优化<br />
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-content lmhp388r2a-editor_css',
+      },
+      {
+        name: 'button',
+        children: {
+          href:
+            'https://www.bilibili.com/video/BV1mW4y1i7dY/?spm_id_from=333.999.0.0&vd_source=9a8e9fb48f82546fa4a631dba8d8357d',
+          type: 'primary',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <p>点击跳转</p>
+                </span>
+              </span>
+            </span>
+          ),
+          target: '_blank',
+        },
+        className: 'lmhp49w5gxq-editor_css',
+      },
+    ],
+  },
+};
+export const Pricing03DataSource = {
+  wrapper: { className: 'home-page-wrapper pricing0-wrapper' },
+  OverPack: { playScale: 0.3, className: 'home-page pricing0', always: false },
+  imgWrapper: { className: 'pricing0-img-wrapper', md: 12, xs: 24 },
+  img: {
+    className: 'pricing0-img',
+    name: 'image',
+    children:
+      'https://raw.githubusercontent.com/Chenzixi0504/Chenzixi0504.github.io/main/AAAAAAAAAABjARQnAQ.png',
+  },
+  childWrapper: {
+    className: 'pricing0-text-wrapper',
+    md: 12,
+    xs: 24,
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <p>
+                      <span>
+                        <br />
+                      </span>
+                    </p>
+                    <p>
+                      <span>雕刻案例</span>
+                      <br />
+                    </p>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-title lmhpsnk6ryt-editor_css',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <span>
+              <p>对相关雕刻软件进行初步学习并完成若干模型的高模制作</p>
+              <p>
+                <br />
+              </p>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-content lmhptb4dbs-editor_css',
+      },
+      {
+        name: 'button',
+        children: {
+          href: '#',
+          type: 'primary',
+          children: (
+            <span>
+              <p>暂无跳转</p>
+            </span>
+          ),
+          className: 'lmhpurbij2-editor_css',
+        },
+        className: 'lmhptbmq05e-editor_css',
+      },
+    ],
+  },
+};
+export const Feature10DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: {
+    className: 'home-page content1',
+    playScale: 0.3,
+    appear: true,
+    always: false,
+  },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children:
+      'https://raw.githubusercontent.com/Chenzixi0504/Chenzixi0504.github.io/main/20230913122708.png',
+    className: 'lmh8c5ovvaf-editor_css',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: {
+    className: 'content1-title',
+    children: (
+      <span>
+        <span>
+          <span>
+            <p>联系方式</p>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  content: {
+    className: 'content1-content lmh894kgy0i-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <p>电话：15816159055（微信同号）</p>
+            <p>邮箱：JennyBakery@yeah.net</p>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
+export const Nav31DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header3-logo lmh5qj4zcpu-editor_css',
+    children:
+      'https://gw.alipayobjects.com/zos/basement_prod/b30cdc2a-d91c-4c78-be9c-7c63b308d4b3.svg',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header3-item lmh5r589809-editor_css',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: '导航一',
+              name: 'text',
+              className: 'lmh5qqutgc7-editor_css',
+            },
+          ],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+          {
+            name: 'sub1',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: '导航二',
+              name: 'text',
+              className: 'lmh5qstjx2-editor_css',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航三', name: 'text' }],
+          className: 'lmh5qv1g88s-editor_css',
+        },
+      },
+      {
+        name: 'item3',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: '导航四',
+              name: 'text',
+              className: 'lmh5qx6cchm-editor_css',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Footer00DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper footer0-wrapper lmh5v2ksjg-editor_css',
+  },
+  OverPack: {
+    className: 'home-page footer0 lmh5uo16lo-editor_css',
+    playScale: 0.05,
+  },
+  copyright: {
+    className: 'copyright lmh5oncvchm-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              ©2023&nbsp;<a href="https://motion.ant.design">Chenzixi</a>&nbsp;All
+              Rights Reserved
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
